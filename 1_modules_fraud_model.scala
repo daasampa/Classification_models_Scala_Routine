4a5fa59a787ed656dc06971d019d4105c0b9e1e4
David Arango Sampayo <39955504+daasampa@users.noreply.github.com>
2018-06-05 14:52:08 -0500

    Update 1_modules_fraud_model.scala
    Guidelines of the scala classes requiered before any step in the API execution.

--- 1_modules_fraud_model.scala
+++ 1_modules_fraud_model.scala
@@ -1,3 +1,5 @@
+/*Scala classes needed to transform data and implement the estimation procedure for the classification statistical methods
+using the stage-defined pipeline model.*/
 import org.apache.spark.ml.feature.{VectorAssembler}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{DecisionTreeClassifier, GBTClassifier, RandomForestClassifier, NaiveBayes, LogisticRegression}
