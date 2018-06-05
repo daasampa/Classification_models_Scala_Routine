9e1411595031b4c56f285b36700c6c0c61d7681a
David Arango Sampayo <39955504+daasampa@users.noreply.github.com>
2018-06-05 14:33:34 -0500

    Update Executions.scala

--- Executions.scala
+++ Executions.scala
@@ -1,3 +1,6 @@
+/*Inside the edge node you must have this three (3) .scala scripts. Using MobaXterm you can easily create folders, name them as you wish.
+Within a spark shell session the load instance calls all the objects within the files.
+The execution order is suggested by the numbers at the beggining.*/
 :load "/home/daasampa/Binary_classificator_model/1_modules_fraud_model.scala"
 :load "/home/daasampa/Binary_classificator_model/2_data_set_fraud_model.scala"
 :load "/home/daasampa/Binary_classificator_model/3_fraud_model.scala"
