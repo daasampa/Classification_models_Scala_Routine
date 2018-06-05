44177a318b600b8f9926ac3c5dc0f8bc08875821
David Arango Sampayo <39955504+daasampa@users.noreply.github.com>
2018-06-05 15:09:43 -0500

    Update 2_data_set_fraud_model.scala
    Guidelines to import from HDFS the expected data to implement the "learning" algorithms.

--- 2_data_set_fraud_model.scala
+++ 2_data_set_fraud_model.scala
@@ -1,2 +1,5 @@
+/*Through the spark.sql method it's possible to call data stored in HDFS and give it the structure of Spark dataframe (org.apache.spark.sql.DataFrame).
+The base_model, default name for this object, needs a sql sentece to retrieve the data.*/
 val base_modelo = spark.sql("""select * from proceso_seguridad_externa.arl_base_fraude_mdp where documento is not null""")
+/*The following command distribute the data available in base_modelo through all the slaves set.*/
 base_modelo.cache()
